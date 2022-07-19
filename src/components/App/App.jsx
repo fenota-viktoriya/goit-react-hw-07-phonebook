3babdc2947979f1e61d1c8e2500d2ece79df7dc9
fenota-viktoriya <Fen9206@gmail.com>
2022-07-19 14:39:05 +0300

    fix: adds components render conditions

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,14 +2,17 @@ import { Contacts } from 'components/Contacts';
 import { Filter } from 'components/Filter';
 import { Form } from 'components/Form';
 import { Container, MainTitle, Title } from './App.styled';
+import { useGetContactsQuery } from '../../redux/contactsSlice';
 
 export function App() {
+  const { status, data } = useGetContactsQuery();
+
   return (
     <Container>
       <MainTitle>Phone book</MainTitle>
       <Form />
-      <Title>Contacts</Title>
-      <Filter />
+      {status === 'fulfilled' && data.length !== 0 && <Title>Contacts</Title>}
+      {status === 'fulfilled' && data.length !== 0 && <Filter />}
       <Contacts />
     </Container>
   );
